c2d4d85730b39b5073484c88322a4a89caf8271c
simple <shwncho4@gmail.com>
2022-01-24 16:22:54 +0900

    학급회장(간단)

--- HashMap, TreeSet/학급 회장/src/Main.java
+++ HashMap, TreeSet/학급 회장/src/Main.java
@@ -2,25 +2,19 @@ import java.util.*;
 
 public class Main {
     public char solution(int n, String s){
-        char answer;
-        char[] c = s.toCharArray();
-        HashMap<Character,Integer> hm = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            char x =c[i];
-            if(hm.containsKey(x))   hm.put(x,hm.get(x)+1);
-            else{
-                hm.put(x,0);
+        char answer=' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char x: s.toCharArray()){
+            map.put(x, map.getOrDefault(x,0)+1);
+        }
+        int max = Integer.MIN_VALUE;
+        for(char key: map.keySet()){
+            if(map.get(key)>max){
+                max=map.get(key);
+                answer=key;
             }
         }
-        Map.Entry<Character,Integer> maxEntry = null;
 
-        Set<Map.Entry<Character,Integer>> entrySet = hm.entrySet();
-        for(Map.Entry<Character,Integer> entry: entrySet){
-            if(maxEntry==null || entry.getValue() > maxEntry.getValue()){
-                maxEntry=entry;
-            }
-        }
-        answer= maxEntry.getKey();
         return answer;
     }
     public static void main(String[] args) {
