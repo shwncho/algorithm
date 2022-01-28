cf0d87e1c17b77cc6cee7fc660cd9e42c949277c
simple <shwncho4@gmail.com>
2022-01-29 00:27:05 +0900

    매출액의 종류

---
+++ HashMap, TreeSet/매출액의 종류/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static ArrayList<Integer> solution(int n, int k, int[] a){
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer,Integer> test = new HashMap<>();
+        int s=0,e=0;
+        for(e=0; e<n; e++){
+            test.put(a[e],test.getOrDefault(a[e],0)+1);
+            if(e>=k-1){
+                answer.add(test.size());
+                if(test.get(a[s])==1) {
+                    test.remove(a[s]);
+                }
+                else{
+                    test.put(a[s],test.get(a[s])-1);
+                }
+                s++;
+            }
+
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int x:solution(n,k,arr)){
+            System.out.print(x+" ");
+        }
+
+    }
+}
--- HashMap, TreeSet/아나그램ver2/src/Main.java
+++ HashMap, TreeSet/아나그램ver2/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
             map.put(x, map.getOrDefault(x,0)+1);
         }
         for(char x:s2.toCharArray()){
+            //key자체가 없거나, -1로 감소 시켰을 때 더이상 없으면 "NO"
             if(!map.containsKey(x) || map.get(x)==0)   return "NO";
             map.put(x, map.get(x)-1);
         }
