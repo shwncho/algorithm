22ed1ae29b02ff5c7696fdd465a20de169d4f74b
simple <shwncho4@gmail.com>
2022-02-15 22:13:37 +0900

    modyfy bubble sort

--- Sorting and Searching/버블정렬/src/Main.java
+++ Sorting and Searching/버블정렬/src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static int[] solution(int n, int[] arr){
         int tmp=0;
-        for(int i=0; i<n-1; i++){
-            for(int j=1; j<n; j++){
-                if(arr[j]<arr[j-1]){
+        for(int i=1; i<n; i++){
+            for(int j=0; j<n-i; j++){
+                if(arr[j]>arr[j+1]){
                     tmp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=tmp;
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
                 }
             }
         }
