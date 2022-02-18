482ac62b0f29eb8865a97055fccdb4acb63146e8
simple <shwncho4@gmail.com>
2022-02-18 18:40:28 +0900

    재귀함수(스택프레임)

---
+++ Recursive, Tree, Graph/재귀함수(스택프레임)/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void DFS(int n){
+        if(n==0) return;
+        else{
+            DFS(n-1);
+            System.out.print(n+" ");
+        }
+    }
+    public static void main(String[] args) {
+        DFS(3);
+    }
+}
