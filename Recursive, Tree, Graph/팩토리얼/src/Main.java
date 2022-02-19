5c609869afdc3c1d248eb24d65dee978e54f21b1
simple <shwncho4@gmail.com>
2022-02-19 17:10:20 +0900

    팩토리얼

---
+++ Recursive, Tree, Graph/팩토리얼/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static int DFS(int n){
+        if(n==1)    return 1;
+
+        return n*DFS(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(DFS(5));
+    }
+}
