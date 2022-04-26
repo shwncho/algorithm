f2eb4812e989fc284620f9db848622d4ed2e25cd
simple <shwncho4@gmail.com>
2022-04-26 19:25:32 +0900

    블랙잭

---
+++ BOJ/2798/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0; i<n-2; i++){
+            for(int j=i+1; j<n-1; j++){
+                for(int k=j+1; k<n; k++){
+                    int temp=arr[i]+arr[j]+arr[k];
+                    if(temp<=m)   list.add(temp);
+                }
+            }
+        }
+
+        System.out.println(Collections.max(list));
+    }
+}
