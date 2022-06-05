7f06683c09ead2b439c5b297acc9261b67f45b9b
simple <shwncho4@gmail.com>
2022-06-05 16:27:34 +0900

    소수 찾기(반복횟수 1000번 -> Math.sqrt(1000) 줄이기)

--- BOJ/1978/src/Main.java
+++ BOJ/1978/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         arr[0]=arr[1]=1;
 
-        for(int i=2; i<=1000; i++){
+        for(int i=2; i<=Math.sqrt(1000); i++){
             if(arr[i]==1)   continue;
             for(int j=2*i; j<=1000; j=j+i){
                 arr[j]=1;
