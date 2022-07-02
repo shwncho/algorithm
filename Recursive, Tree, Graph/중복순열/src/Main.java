fb5ed2390c9361954d59b40d35dc335047c3c84e
simple <shwncho4@gmail.com>
2022-07-03 07:41:28 +0900

    중복순열

---
+++ Recursive, Tree, Graph/중복순열/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] pm;
+    static int n, m;
+    public void DFS(int L){
+        if(L==m){
+            for (int x : pm) {
+                System.out.print(x+" ");
+            }
+            System.out.println();
+        }
+        else{
+            for(int i=1; i<=n; i++){
+                pm[L]=i;
+                DFS(L+1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        pm=new int[m];
+        T.DFS(0);
+    }
+}
