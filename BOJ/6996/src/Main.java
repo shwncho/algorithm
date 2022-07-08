2d6ba483adcf09b623fcce2805f089eb59a02223
simple <shwncho4@gmail.com>
2022-07-08 23:08:44 +0900

    애너그램.ver2

--- BOJ/6996/src/Main.java
+++ BOJ/6996/src/Main.java
@@ -1,46 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static boolean solution(String s1, String s2){
+
+    private static boolean solveAnagrams(String first, String second ) {
+        /* ------------------- INSERT CODE HERE --------------------
+         *
+         * Your code should return true if the two strings are anagrams of each other.
+         *
+         * */
         HashMap<Character,Integer> h1=new HashMap<>();
         HashMap<Character,Integer> h2=new HashMap<>();
 
-        for(Character x : s1.toCharArray()){
+        for(char x : first.toCharArray()){
             h1.put(x, h1.getOrDefault(x,0)+1);
         }
 
-        for(Character x : s2.toCharArray()){
+        for(char x : second.toCharArray()){
             h2.put(x, h2.getOrDefault(x,0)+1);
         }
 
-        for(Character x : h1.keySet()){
+        for(char x : h1.keySet()){
             if(!h1.get(x).equals(h2.get(x)))    return false;
         }
 
         return true;
 
+        /* -------------------- END OF INSERTION --------------------*/
     }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s1,s2;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(br.readLine());
+        int numTests = sc.nextInt();
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < numTests; i++) {
+            String first = sc.next().toLowerCase();
+            String second = sc.next().toLowerCase();
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            s1=st.nextToken();
-            s2=st.nextToken();
-
-            System.out.println(s1+" & " + s2+" are " + (solution(s1,s2) ? " anagrams.":" NOT anagrams."));
-
-
+            System.out.println(first + " & " + second + " are " + (solveAnagrams(first, second) ? "anagrams." : "NOT anagrams."));
         }
-
     }
 }
