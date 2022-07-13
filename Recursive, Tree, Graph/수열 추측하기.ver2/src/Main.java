6b05d80b88938719e16fb17a9a3ba029e07178cb
simple <shwncho4@gmail.com>
2022-07-13 20:48:59 +0900

    수열 추측하기.ver2

---
+++ Recursive, Tree, Graph/수열 추측하기.ver2/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] b, p, ch; // b는 조합 nCo 부터 nCn까지 담을 배열, p는 순열이 담길 배열, ch는 담겼는지 여부를 check
+    static int n, f;
+    boolean flag = false;
+    int[][] dy = new int[35][35];
+    public int combi(int n, int r){
+        if(dy[n][r]>0)   return dy[n][r];
+        if(n==r || r==0)    return 1;
+        else return dy[n][r]=combi(n-1,r-1)+combi(n-1,r);
+    }
+
+    public void DFS(int L, int sum){
+        if(flag)    return;
+        if(L==n && sum==f){
+            for(int x : p) System.out.print(x+" ");
+            flag=true;
+        }
+        else{
+            for(int i=1; i<=n; i++){
+                if(ch[i]==0){
+                    ch[i]=1;
+                    p[L]=i;
+                    DFS(L+1,sum+(p[L]*b[L]));
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        f = sc.nextInt();
+        b = new int[n];
+        p = new int[n];
+        ch = new int[n+1];
+        for(int i=0; i<n; i++){
+            b[i]=T.combi(n-1,i);
+        }
+        T.DFS(0,0);
+    }
+}
