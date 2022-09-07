4274e52eaf2338287442c86991cb367ffde4943a
simple <shwncho4@gmail.com>
2022-09-07 15:04:25 +0900

    가장 긴 증가하는 부분 수열

---
+++ BOJ/11053/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dp;
+    public static int solution(int[] arr, int n){
+        int answer=Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+            dp[i]=1;
+            for(int j=0; j<=i; j++){
+                if(arr[i]>arr[j])   dp[i]=Math.max(dp[i],dp[j]+1);
+            }
+            answer=Math.max(answer,dp[i]);
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        dp=new int[n];
+
+        st=new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(arr,n));
+    }
+}
