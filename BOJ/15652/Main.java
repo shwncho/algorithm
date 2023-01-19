16a473c7ba45f7b7e789e341edf6a62ecde74078
simple <shwncho4@gmail.com>
2023-01-19 19:42:06 +0900

    BOJ

--- BOJ/15652/Main.java
+++ BOJ/15652/Main.java
@@ -5,29 +5,21 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n,m;
-    static int[] ch,pm;
+    static int[] ch,pm,arr;
     static StringBuilder sb=new StringBuilder();
-    static void DFS(int L, int[] arr){
+    static void DFS(int L, int s){
         if(L==m){
-            if(isIncreasingOrder(pm)){
-                for (int x : pm) {
-                    sb.append(x+" ");
-                }
-                sb.append("\n");
+            for (int x : pm) {
+                sb.append(x+" ");
             }
+            sb.append("\n");
         }
         else{
-            for(int i=0; i<n; i++){
+            for(int i=s; i<n; i++){
                 pm[L]=arr[i];
-                DFS(L+1,arr);
+                DFS(L+1,i);
             }
         }
-    }
-    static boolean isIncreasingOrder(int[] arr){
-        for(int i=0; i<arr.length-1; i++){
-            if(arr[i]>arr[i+1]) return false;
-        }
-        return true;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,12 +27,12 @@ public class Main {
         n=Integer.parseInt(st.nextToken());
         m=Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        arr = new int[n];
         ch=new int[n];
         pm=new int[m];
         for(int i=0; i<n; i++)  arr[i]=i+1;
 
-        DFS(0,arr);
+        DFS(0,0);
         System.out.println(sb);
     }
 }
