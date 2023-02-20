43ed9f51fc14c61da570e8048f4e70e668fe681f
simple <shwncho4@gmail.com>
2023-02-20 17:08:36 +0900

    BOJ

---
+++ BOJ/15988/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[1000001];
+
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=4;
+        for(int i=4; i<=1000000; i++){
+            dp[i]=(dp[i-1]+dp[i-2]+dp[i-3])%1000000009;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<t; i++){
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
