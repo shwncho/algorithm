45c08d6dd0d4763f838b121924ce1172d683932e
simple <shwncho4@gmail.com>
2024-04-15 15:18:20 +0900

    codility

---
+++ codility/TieRopes/Solution.java
@@ -0,0 +1,22 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int K, int[] A) {
+        // Implement your solution here
+        int answer = 0;
+        int sum = 0;
+        for(int i=0; i<A.length; i++){
+            sum+=A[i];
+            if(sum>=K){
+                answer++;
+                sum=0;
+            }
+        }
+
+        return answer;
+    }
+}
