3b887faa72d771a3e84599af3df3f3c83833ee5d
shwncho <shwncho4@gmail.com>
2024-11-14 17:42:17 +0900

    BOJ

---
+++ BOJ/10869/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(A+B).append("\n");
+        sb.append(A-B).append("\n");
+        sb.append(A*B).append("\n");
+        sb.append(A/B).append("\n");
+        sb.append(A%B).append("\n");
+
+        System.out.println(sb);
+    }
+}
