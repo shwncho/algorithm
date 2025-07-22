39df02ed672876f6550db881db5a49826cc488d9
shwncho <shwncho4@gmail.com>
2025-07-22 21:03:18 +0900

    leetcode

---
+++ leetcode/Merge Strings Alternately/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int left = 0;
+        int right = 0;
+
+        while(left < word1.length() && right < word2.length()) {
+            sb.append(word1.charAt(left++));
+            sb.append(word2.charAt(right++));
+        }
+
+        while(left < word1.length())  sb.append(word1.charAt(left++));
+        while(right < word2.length()) sb.append(word2.charAt(right++));
+
+        return sb.toString();
+    }
+}
