6c5dfec4e0839b5507a3c66265eaaed8eba27f30
shwncho <shwncho4@gmail.com>
2025-08-30 17:51:33 +0900

    leetcode

---
+++ leetcode/Find Pivot Index/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] sum = new int[nums.length+1];
+        for(int i=1; i<=nums.length; i++){
+            sum[i] = sum[i-1] + nums[i-1];
+        }
+        for(int i=0; i<nums.length; i++){
+            if(sum[i] == (sum[nums.length]-sum[i+1])) return i;
+        }
+
+        return -1;
+    }
+}
