14d64ce95223097e25d13533be49b0ad63700bfd
shwncho <shwncho4@gmail.com>
2026-01-16 14:49:48 +0900

    leetcode

---
+++ leetcode/Find the Peaks/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> answer = new ArrayList<>();
+
+        for(int i=1; i<mountain.length-1; i++) {
+            if(mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1])  answer.add(i);
+        }
+
+        return answer;
+    }
+}
