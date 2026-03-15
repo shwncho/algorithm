dd35de158b3334af5bef56dc609ea690c3c99781
shwncho <shwncho4@gmail.com>
2026-03-15 22:45:42 +0900

    leetcode

---
+++ leetcode/Find First Palindromic String in the Array/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for(String s : words) {
+            if(isPalindrome(s)) return s;
+        }
+
+        return "";
+    }
+    private boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start<=end) {
+            if(s.charAt(start++) != s.charAt(end--))    return false;
+        }
+
+        return true;
+    }
+}
