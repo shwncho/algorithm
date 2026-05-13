64e6a3dca288740d71a07c288673f7a38f3b674d
shwncho <shwncho4@gmail.com>
2026-05-13 19:39:15 +0900

    BOJ

---
+++ BOJ/2631/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+
+        int max = 0;
+        for(int i=0; i<n; i++) {
+            dp[i] = 1;
+            for(int j=i-1; j>=0; j--) {
+                if(arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(n-max);
+    }
+}
